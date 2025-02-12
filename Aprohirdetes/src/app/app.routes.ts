76fd1092ba6919aa616d1b246860d07362043ec1
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2025-02-12 11:09:17 +0100

    routes fix

--- Aprohirdetes/src/app/app.routes.ts
+++ Aprohirdetes/src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component'; 
-import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-    { path: '/home', component: HomeComponent },
-    { path: 'log', component: LoginComponent },
-
-    {
-        path: '', redirectTo: 'home', pathMatch: 'full'
-      },
-
+    {path: 'login' , component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
 ];
