eebdaeaacecb044b27e6c6bb639ba8902ecdc635
auerzoltan <80397127+auerzoltan@users.noreply.github.com>
2025-02-12 11:50:34 +0100

    regisztráció oldal elkészítése

--- Aprohirdetes/src/app/components/register/register.component.ts
+++ Aprohirdetes/src/app/components/register/register.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [    
+    CommonModule,
+    MatSlideToggleModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
